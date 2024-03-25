ec266e7fdaafb61c45a48a23c648568e87f097f5
Lavynia de Oliveira <lavynia.oliveira@unifei.edu.br>
2024-03-25 14:57:02 -0300

    feat: navbar color hover and contact icons

--- components/ContactBody/contactBody.tsx
+++ components/ContactBody/contactBody.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 export default function ContactBody() {
-    return (
-        <main>
-            <section className="text-center text-sky-100">
-                <h1 className="text-3xl font-montserrat mt-40 font-semibold">CONTACT</h1>
-                <h2 className="text-[50px] font-zilla font-bold leading-none mt-14">Let’s keep in touch.</h2>
-                <p className="text-[16px] font-montserrat">lavynia.oliveira@unifei.edu.br</p>
-            </section>
-        </main>
-    )
+  return (
+    <main>
+      <section className="text-center text-sky-100">
+        <h1 className="text-3xl font-montserrat mt-40 font-semibold">
+          CONTACT
+        </h1>
+        <h2 className="text-[50px] font-zilla font-bold leading-none mt-14">
+          Let’s keep in touch.
+        </h2>
+        <p className="text-[16px] font-montserrat">
+          lavynia.oliveira@unifei.edu.br
+        </p>
+          <div className="flex justify-center">
+            <a href="https://github.com/lavyoliveira">
+                <FontAwesomeIcon icon={faGithub} className="w-10 h-10"/>
+            </a>
+            <a href="https://www.linkedin.com/in/lavynia-de-oliveira-2b121322b/">
+                <FontAwesomeIcon icon={faLinkedin} className="w-10 h-10"/>
+              </a>
+          </div>
+      </section>
+    </main>
+  );
 }
--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -1,15 +1,32 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Header() {
+  const [linkActive, setLinkActive] = useState("/");
+  const router = useRouter();
+
+  useEffect(() => {
+    const activeLink = router.pathname;
+    setLinkActive(activeLink);
+  }, [router.pathname]);
+
   return (
-    <header className="bg-black text-sky-100 pt-20 px-10 font-zilla-slab font-bold text-[20px] w-full max-w-7xl">
+    <header className="bg-black text-sky-100 pt-20 px-10 font-zilla font-bold text-[20px] w-full max-w-7xl">
       <nav className="flex justify-between container">
-        <Link href="/">home</Link>
+        <Link href="/" className={`transition duration-500 ease-in-out cursor-pointer ${
+          linkActive === "/" ? "text-orange-500" : ""
+        }`}>home</Link>
         <div className="flex space-x-4">
-          <Link href="/about">about</Link>
-          <Link href="/projects">projects</Link>
-          <Link href="/contact">contact</Link>
+          <Link href="/about" className={`transition duration-500 ease-in-out cursor-pointer ${
+              linkActive === "/about" ? "text-orange-500" : ""
+            }`}>about</Link>
+          <Link href="/projects" className={`transition duration-500 ease-in-out cursor-pointer ${
+              linkActive === "/projects" ? "text-orange-500" : ""
+            }`}>projects</Link>
+          <Link href="/contact" className={`transition duration-500 ease-in-out cursor-pointer ${
+              linkActive === "/contact" ? "text-orange-500" : ""
+            }`}>contact</Link>
         </div>
       </nav>
     </header>
